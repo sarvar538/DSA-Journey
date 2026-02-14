13b420f6416cb07c0667bd4751b46e4ab830217c
sarvar538 <sarvarmansuri77@gmail.com>
2026-02-14 23:14:33 +0530

    Create two-sum.cpp

---
+++ DSA/Day-01/two-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //By hashing - logic 
+        unordered_map < int , int > m ;
+        for ( int i = 0 ; i < nums.size(); i++ ) {
+            int need = target - nums[i];
+            if ( m.count(need)) {
+               return { m[need] , i } ;
+            }
+            m[nums[i]] = i ;
+        }
+        return {};
+    }
+};
