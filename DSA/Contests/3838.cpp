b7b6bbc6e8d1da25529e66c0303289278eed2850
sarvar538 <sarvarmansuri77@gmail.com>
2026-02-16 21:39:34 +0530

    Create 3838.cpp

---
+++ DSA/Contests/3838.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans = "";
+        for ( int i = 0 ; i < words.size(); i++ ) {
+            int sum = 0 ;
+            for ( int j = 0 ; j < words[i].size(); j++ ) {
+                char c = words[i][j];
+                int index = c - 'a';
+                sum = sum + weights[index];
+            }
+            int mod = sum % 26 ;
+            char mapping = 'z' - mod ;
+            ans.push_back(mapping);
+        }
+        return ans ;
+    }
+};
