24be1143301b18b34bedb072fcbb7d592f742537
sarvar538 <sarvarmansuri77@gmail.com>
2026-02-16 21:14:08 +0530

    Create 53.cpp

---
+++ DSA/Day-03/53.cpp
@@ -0,0 +1,33 @@
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+//         int maxSum = INT_MIN ;
+//         for ( int i = 0 ; i < nums.size(); i++ ) {
+//             int currSum = 0 ;
+//             for ( int j = i ; j < nums.size(); j++ ) {
+//                 currSum += nums[j];
+//                 maxSum = max ( currSum , maxSum ) ;
+//             }
+//         }
+//         return maxSum;
+//     }
+// };
+
+
+
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       int currSum = 0 , maxSum = INT_MIN ;
+       for ( int i = 0 ; i < nums.size(); i++ ) {
+          currSum += nums[i];
+          maxSum = max ( currSum , maxSum );
+          if ( currSum < 0 ) {
+            currSum = 0 ;
+          }
+       }
+       return maxSum ;
+    }
+};
